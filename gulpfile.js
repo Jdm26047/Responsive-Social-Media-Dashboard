f82602232d7f8f7d8ca9547acb5b234fb8ee5454
Jordn <jdm26047@email.vccs.edu>
2025-04-23 18:18:30 -0400

    Found it smh
    at some point i read map.get
    was the appropriate selector in the breakpoints.scss file.
    
    Wrong
    
    Map-get was the correct entry.
    I learned not to be so hasty to apply information i havent properly meditated on, just to move along the process.

--- gulpfile.js
+++ gulpfile.js
@@ -60,5 +60,5 @@ function watchTask() {
 // Default Gulp Task
 exports.default = series(scssTask, jsTask, browserSyncServe, watchTask); 
 
-// Build Gulp Task
-exports.build = series(scssTask, jsTask);
+// // Build Gulp Task
+// exports.build = series(scssTask, jsTask);
